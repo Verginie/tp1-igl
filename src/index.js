3f54f5ffba355642361b4d0782e3fb0da889cdd3
yasserchouadra <43786517+Yasserchouadra@users.noreply.github.com>
2019-11-25 00:07:34 +0100

    index.js changement
    this is the new index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,49 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import 'jquery/src/jquery';
 import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/js/bootstrap.js';
 import "./assets/style.css"
 import AuthentificationForm from './components/authentificationForm';
 import Footer from './components/footer';
+import Home from './components/Home';
 
-ReactDom.render(<AuthentificationForm/>,document.getElementById('root'));
-ReactDom.render(<Footer/>,document.getElementById('footer'));
+//ReactDom.render(<AuthentificationForm/>,document.getElementById('root'));
+// ReactDom.render(<Footer/>,document.getElementById('footer'));
+
+class App extends React.Component {
+
+    state={
+      
+       comptes :[
+        { id: 1,mail : "yasser", password :"yasser",tab:[{id_annee :2,id_group :5},{id_annee :2,id_group :7}]},
+        { id: 2,mail : "taki", password :"taki",tab:[{id_annee :1,id_group :1},{id_annee :1,id_group :2}] },
+        { id: 3, mail : "bch", password :"bch" ,tab:[{id_annee :3,id_group :7},{id_annee :3,id_group :8}]}
+                ],
+          }
+
+        logini=(email,pswd) => {
+
+            const comptes = [...this.state.comptes];
+              comptes.map(compte =>{
+                       if(compte.mail === email && compte.password === pswd){
+                       ReactDom.render(<Home my_compte={compte} />,document.getElementById('root'));
+              }
+            })
+              
+
+                                };
+      render() {
+      return (
+        <div>
+            <ul>
+            <AuthentificationForm fonction1={this.logini} />      
+            </ul>
+        <Footer/>
+        </div>
+      );
+    }
+  }
+  
+  const rootElement = document.getElementById("root");
+  ReactDom.render(<App />, rootElement);
