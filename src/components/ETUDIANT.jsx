4bbdcb866491216e424f15ac083264e375ed8819
yasserchouadra <43786517+Yasserchouadra@users.noreply.github.com>
2019-11-27 20:22:15 +0100

    formulaire

--- src/components/ETUDIANT.jsx
+++ src/components/ETUDIANT.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+
+//<td><button onClick ={this.cc}> ok </button> </td>
+
 class ETUDIANT extends Component {
     
         state={
@@ -8,22 +11,43 @@ class ETUDIANT extends Component {
           note_cf :0,
           moyen :0        
            }
-           ecrire1 = event => {
-            this.setState({ note_cc: event.currentTarget.value });
-            this.props.item.Note_cc=event.currentTarget.value;       
-                      };
-          ecrire2 = event => {
-            this.setState({ note_ci: event.currentTarget.value });
 
-            this.props.item.Note_ci=event.currentTarget.value;
-            
+
+           ecrire1 = event => {
+            this.setState({ note_cc: event.currentTarget.value});
+            this.props.item.Note_cc = parseInt(this.state.note_cc, 10) ;
+            this.setState({ 
+               moyen : (
+                 (this.state.moyen * 3 - parseInt(this.state.note_cc, 10)) + 
+                 parseInt(event.currentTarget.value, 10)
+                        ) *(1/3) });                   
+                              };
+          ecrire2 = event => {
+            this.setState({ note_ci: event.currentTarget.value });   
+            this.props.item.Note_ci = parseInt(this.state.note_ci, 10) ;
+
+            this.setState({ 
+              moyen : (
+                (this.state.moyen * 3 - parseInt(this.state.note_ci, 10)) + 
+                parseInt(event.currentTarget.value, 10)
+                       ) *(1/3) }); 
+
           };
         
           ecrire3 = event => {
             this.setState({ note_cf: event.currentTarget.value });
-      
-            this.props.item.Note_cf =event.currentTarget.value;
-          };   
+            this.props.item.Note_cf = parseInt(this.state.note_cf, 10) ;
+
+            this.setState({ 
+              moyen : (
+                (this.state.moyen * 3 - parseInt(this.state.note_cf, 10)) + 
+                parseInt(event.currentTarget.value, 10)
+                       ) *(1/3) }); 
+                 };
+           
+         
+           
+         
     render() { 
 
         return  (
@@ -31,13 +55,12 @@ class ETUDIANT extends Component {
                     <tr size ='255'>
                         <th scope="row">{this.props.item.matricule}</th>
                             <td>{this.props.item.nom}</td>
-                            <td>{this.props.item.prenom}</td>
-                           
-                            <td><input type="number" max="20" class="form-control" id="noteCC" placeholder="------"   value={this.state.note_cc}  onChange={this.ecrire1}   ></input></td>
-                            <td><input type="number" max="20" class="form-control" id="noteCI" placeholder="-----"   value={this.state.note_ci}  onChange={this.ecrire2}  ></input></td>
-                            <td><input type="number" max="20" class="form-control" id="noteCF" placeholder="-----"   value={this.state.note_cf}  onChange={this.ecrire3}  ></input></td>
-                            <td><input type="number" max="20" class="form-control" id="moyen" placeholder="------"  value={this.props.MY_moyen}></input>  </td>
-
+                            <td>{this.props.item.prenom}</td>  
+                            <td><input type="number" min ="0" max="20" class="form-control" id="noteCC" placeholder=""            value={this.state.note_cc}  onChange={this.ecrire1}   ></input></td>
+                            <td><input type="number" min ="0" max="20" class="form-control" id="noteCI" placeholder=""            value={this.state.note_ci}  onChange={this.ecrire2}  ></input></td>
+                            <td><input type="number" min ="0" max="20" class="form-control" id="noteCF" placeholder=""            value={this.state.note_cf}  onChange={this.ecrire3}  ></input></td>
+                            <td><h1> {this.state.moyen} </h1> </td>
+  
                         <td>--</td>
         
                     </tr>
