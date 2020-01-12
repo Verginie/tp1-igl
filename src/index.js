0626b293e8c129b6d2fabd747250d6559fd6ed56
yasserchouadra <43786517+Yasserchouadra@users.noreply.github.com>
2020-01-12 07:00:37 +0100

    AJOUT
    l'ajout du index.js

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import 'jquery/src/jquery';
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/js/bootstrap.js';
+import "./assets/style.css"
+import AuthentificationForm from './components/authentificationForm';
+import Table from './components/Home';
+import Home from './components/Home';
+
+import axios from 'axios';
+
+
+class App extends React.Component {
+
+  state={
+    comptes :[]
+     
+        }
+       
+///////////////////////////////////////////////////////////////////////////////
+                  componentDidMount(){
+                      axios.get('http://localhost:5000/teacher/')
+                         .then( teachers => {
+                                                this.setState({ comptes : teachers.data })   
+                                           })
+                         .catch(function (error) { console.log(error);})
+                                       }     
+///////////////////////////////////////////////////////////////////////////////
+
+       ///////////////////////////////////////////////////////////////////////////////
+
+       logini=(email,pswd) => {
+        const comptes = [...this.state.comptes];
+         comptes.map(compte =>{
+           if(compte.mail === email && compte.password === pswd){
+               ReactDom.render(<Home my_compte={compte} />,document.getElementById('root'));
+         }
+ })
+};
+///////////////////////////////////////////////////////////////////////////////
+      render() {
+      return (
+        <div>
+            
+              <AuthentificationForm fonction1={this.logini} />      
+            
+        
+        </div>
+      );
+    }
+  }
+  
+  const rootElement = document.getElementById("root");
+  ReactDom.render(<App />, rootElement);
